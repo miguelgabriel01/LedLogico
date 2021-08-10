d6c5e3e58b77c9104075d5b7151abd7281bbdf6c
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-08-10 12:56:34 -0300

    Criação de funcão para alternar os led( incialmente )

--- src/components/Inicio.svelte
+++ src/components/Inicio.svelte
@@ -68,86 +68,76 @@
     ];
 
     let cubosAtivos = []; //array para armazenar quais cubos estão ativos no momento
+    //valores dos lados dos cubos
+    const ladoEsquerdoIndex = [0, 8, 16, 24, 32, 40, 48, 56]; //indices do lado esquerdo
+    const ladoDireitoIndex = [7, 15, 23, 31, 39, 47, 55, 63]; //inices do lado direito
 
     //cores padrão dos cubos
     let corCuboSelecionado = "#0a2f35"; //cor do cubo selecionado
     let corCubospadrao = "whitesmoke"; //valor inicial do cubo
 
+    //função que verifica os valores recebidos e ativa ou desativa os cubos
+    function handleAtivo(id) {
+        //verificamos se o valor recebido como parametro está no lado esquerdo usando o id recebido como parametro
+        if (ladoEsquerdoIndex.indexOf(id) > -1) {
+            //alert("entrou no lado esquerdo: " + id);
+            cubosAtivos = [id, id + 1, id + 8, id - 8];
+            for (var i = 0; i < cubosAtivos.length; i++) {
+                // alert(cubosAtivos[i]);
+
+                //condição para verificar e não permitir que novos cubos sejam criados
+                if (cubosAtivos[i] >= 0 && cubosAtivos[i] <= 63) {
+                    cubosId[cubosAtivos[i]] = true; //fazemos a atribuição dos valores de acordo com os indeces que foram gerados
+                }
+            }
+            //alert("cubosAtivos++: " + cubosAtivos);
+        }
+        //verificamos se o valor recebido como parametro está no lado direito usando o id recebido como parametro
+        else if (ladoDireitoIndex.indexOf(id) > -1) {
+            //alert("entrou no lado direito: " + id);
+            cubosAtivos = [id, id - 1, id + 8, id - 8];
+            for (var i = 0; i < cubosAtivos.length; i++) {
+                //alert(cubosAtivos[i]);
+
+                //condição para verificar e não permitir que novos cubos sejam criados
+                if (cubosAtivos[i] >= 0 && cubosAtivos[i] <= 63) {
+                    cubosId[cubosAtivos[i]] = true; //fazemos a atribuição dos valores de acordo com os indeces que foram gerados
+                }
+            }
+        }
+        //se não estiver nem no lado direito ou no lado esquerdo, ele estará no meio
+        else {
+            //alert("entrou no meio");
+            cubosAtivos = [id, id - 1, id + 1, id - 8, id + 8];
+            for (var i = 0; i < cubosAtivos.length; i++) {
+                //alert("valor do else " + cubosAtivos[i]);
+                //console.log("entrou no meio/ valores: " + cubosAtivos[i])
+                //console.log('busca de valores do array original ' + cubosId.indexOf(cubosAtivos[i]))
+
+                //condição para verificar e não permitir que novos cubos sejam criados
+                if (cubosAtivos[i] >= 0 && cubosAtivos[i] <= 63) {
+                    cubosId[cubosAtivos[i]] = true; //fazemos a atribuição dos valores de acordo com os indeces que foram gerados
+                }
+            }
+            //alert("valor do else " + cubosAtivos);
+        }
+    }
+
     //função que recebe o evento que diz qual cubo foi clicado pelo usuario
     function handleClickCubo(id) {
-        //alert("O indice clicado foi: " + id);
-        console.log(id);
-
         /**
         verificamos se o valor recebido como
          parametro é false, se for, alteramos para true e o mesmo acontece se o valor for true
         */
-        let valores = null;
         if (cubosId[id] === false) {
-            //valores dos lados dos cubos
-            const ladoEsquerdoIndex = [0, 8, 16, 24, 32, 40, 48, 56]; //indices do lado esquerdo
-            const ladoDireitoIndex = [7, 15, 23, 31, 39, 47, 55, 63]; //inices do lado direito
-
-            //verificamos se o valor recebido como parametro está no lado esquerdo usando o id recebido como parametro
-            if (ladoEsquerdoIndex.indexOf(id) > -1) {
-                //alert("entrou no lado esquerdo: " + id);
-                cubosAtivos = [id, id + 1, id + 8, id - 8];
-                for (var i = 0; i < cubosAtivos.length; i++) {
-                    // alert(cubosAtivos[i]);
-                 
-                    //condição para verificar e não permitir que novos cubos sejam criados
-                    if (cubosAtivos[i] >= 0 && cubosAtivos[i] <= 63) {
-                        cubosId[cubosAtivos[i]] = true; //fazemos a atribuição dos valores de acordo com os indeces que foram gerados
-                    }
-                }
-                //alert("cubosAtivos++: " + cubosAtivos);
-            }
-            //verificamos se o valor recebido como parametro está no lado direito usando o id recebido como parametro
-            else if (ladoDireitoIndex.indexOf(id) > -1) {
-                //alert("entrou no lado direito: " + id);
-                cubosAtivos = [id, id - 1, id + 8, id - 8];
-                for (var i = 0; i < cubosAtivos.length; i++) {
-                    //alert(cubosAtivos[i]);
-                    
-                    //condição para verificar e não permitir que novos cubos sejam criados
-                    if (cubosAtivos[i] >= 0 && cubosAtivos[i] <= 63) {
-                        cubosId[cubosAtivos[i]] = true; //fazemos a atribuição dos valores de acordo com os indeces que foram gerados
-                    }
-                }
-            }
-            //se não estiver nem no lado direito ou no lado esquerdo, ele estará no meio
-            else {
-                //alert("entrou no meio");
-                cubosAtivos = [id, id - 1, id + 1, id - 8, id + 8];
-                for (var i = 0; i < cubosAtivos.length; i++) {
-                    //alert("valor do else " + cubosAtivos[i]);
-                    //console.log("entrou no meio/ valores: " + cubosAtivos[i])
-                    //console.log('busca de valores do array original ' + cubosId.indexOf(cubosAtivos[i]))
-
-                    //condição para verificar e não permitir que novos cubos sejam criados
-                    if (cubosAtivos[i] >= 0 && cubosAtivos[i] <= 63) {
-                        cubosId[cubosAtivos[i]] = true; //fazemos a atribuição dos valores de acordo com os indeces que foram gerados
-                    }
-                }
-                //alert("valor do else " + cubosAtivos);
-            }
-
-            console.log(
-                "busca de valores do array original 2" + cubosId.indexOf(1)
-            );
-
-            console.log(
-                "valores do cubo aparti da nova implementação: " + cubosAtivos
-            );
-
-            cubosId[id] = true; //informamos que agora o valor do indice de cubos foi alterado para true
+            return handleAtivo(id);
         }
+
         //entra aqui caso o cubo já esteja ativo(true)
         else {
-            cubosId[id] = false; //informamos que agora o valor do indice de cubos foi alterado para false
-            cubosAtivos.push(id); //inserimos na variavel os id que estavam ativos
-            console.log("cubos ativos: " + cubosAtivos);
+            return !handleAtivo(id); 
         }
+
         console.log("cubos ativos 2: " + cubosAtivos);
         console.log("cubosId: " + cubosId);
     }
